042b33984935650c7b957f4da061c7329c5d8b41
景豪 郑 <1534099450@qq.com>
2020-11-15 16:09:23 +0800

    111

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XiangQi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
